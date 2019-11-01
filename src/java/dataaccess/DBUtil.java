3ec5db6a07769f057c676cf6657b5b35cc93ac97
797462 <797462@192.168.118.1>
2019-11-01 13:26:26 -0600

    Week 8 lab Week 8 lab - Added persistence

---
+++ src/java/dataaccess/DBUtil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+/**
+ *
+ * @author 797462
+ */
+public class DBUtil 
+{
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("NotesPU");
+
+    public static EntityManagerFactory getEmFactory() 
+    {
+        return emf;
+    }
+
+}
+    
